a031e4e3cd8dd41eee792f502242e63c7e624d4c
Randy Chou <Randychou2020@gmail.com>
2021-07-01 11:35:33 -0400

    tested and debugged post and get routes

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let reservations = [];
+
+let waitlist = [];
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/home.html')));
 
 app.get('/table', (req, res) => res.sendFile(path.join(__dirname, '/public/tables.html')));
@@ -14,18 +18,19 @@ app.get('/table', (req, res) => res.sendFile(path.join(__dirname, '/public/table
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, '/public/make-res.html')));
 
 app.get('/api/reservations', (req, res) => res.json(reservations));
-app.get('/api/waitlist'), (req, res) => res.json(waitlist)
+
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
 
 app.post('/api/reserve', (req, res) => {
 
     const newReservation = req.body;
 
-    if (reservations <= 5){
-    reservations.push(newReservation);
+    if (reservations.length < 5) {
+        reservations.push(newReservation);
     } else {
         waitlist.push(newReservation)
     }
     res.json(newReservation);
-  });
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT http://localhost:${PORT}`));
