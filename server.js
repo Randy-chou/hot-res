9f039db6be35edd529a536eea052434240b24133
KIMBERLYSEDDON <82526676+KIMBERLYSEDDON@users.noreply.github.com>
2021-07-01 11:00:23 -0400

    some server

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.post('/api/reservations', (req, res) => {
 
+    const newReservation = req.body;
+  
+    newReservation.routeName = newReservation.name.replace(/\s+/g, '').toLowerCase();
+  
+    if (reservations <= 5) {
+    reservations.push(newReservation);
+    } else { waitList.push(newReservation)
+    }
+    res.json(newReservation);
+  });
 
 app.listen(PORT, () => console.log(`App listening on PORT http://localhost:${PORT}`));
